023aacdb7602eb858c5dff4e706903a4f104d2eb
nrjiz <164118742+nrjiz@users.noreply.github.com>
2024-07-05 06:47:03 +0300

    Create B. Basic Data Types.cpp

---
+++ B. Basic Data Types.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+
+int main(){
+    int a;
+    long long b;
+    char c;
+    double d;
+    float f;
+    scanf("%d %lld %c %f %lf",&a,&b,&c,&f,&d);
+    printf("%d  \n%lld  \n%c    \n%.2f  \n%.1lf \n",a,b,c,f,d);
+    return 0;
+    
+}
+
+//5min
+// 3 min 8sec
+//2:45 seconds 
