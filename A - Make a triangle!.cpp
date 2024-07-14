3ce11f358bc332a7f61b7ecad0dc01a63420b992
nrjiz <164118742+nrjiz@users.noreply.github.com>
2024-07-14 18:14:48 +0300

    Update A - Make a triangle!.cpp

--- A - Make a triangle!.cpp
+++ A - Make a triangle!.cpp
@@ -24,8 +24,26 @@ int main(){
         a++;
     }
     printf("%d\n",minutesCounter);
-    
-    
 
+}
 
+///////////////////////////////////////////////////////////////////////////////
+//second way that is more efficient 
+#include<iostream>
+ 
+int main(){
+    
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    int result=0;
+    if(a+b<=c){
+        result+=c-(a+b)+1;
+    }
+    if(a+c<=b){
+        result+=(b-a-c+1);
+    }
+    if(b+c<=a){
+        result+=a-b-c+1;
+    }
+    printf("%d",result);
 }
