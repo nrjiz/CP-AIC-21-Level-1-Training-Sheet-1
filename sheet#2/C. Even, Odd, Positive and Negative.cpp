059fe1ecabbc627b4dc97c32436bf537b86b2d2c
nrjiz <164118742+nrjiz@users.noreply.github.com>
2024-07-17 13:01:30 +0300

    Create C. Even, Odd, Positive and Negative.cpp

---
+++ sheet#2/C. Even, Odd, Positive and Negative.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+int main(){
+    
+    int n;
+    scanf("%d",&n);
+    int counter=n, even=0,odd=0,positive=0,negative=0;
+    while(counter>0){
+        int input;
+        scanf("%d",&input);
+        input%2==0?++even:++odd;
+        input>0?++positive:input==0?:++negative;
+        --counter;
+    }
+    printf("Even: %d\nOdd: %d\nPositive: %d\nNegative: %d\n",even,odd,positive,negative);
+}
