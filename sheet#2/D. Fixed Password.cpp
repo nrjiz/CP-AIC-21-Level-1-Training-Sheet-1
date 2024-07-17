3c649b285a0ae366d0493ffd2c2453681096c6be
nrjiz <164118742+nrjiz@users.noreply.github.com>
2024-07-17 14:01:08 +0300

    Create D. Fixed Password.cpp

---
+++ sheet#2/D. Fixed Password.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+int main(){
+    
+    int pass;
+    while(pass!=1999){
+        scanf("%d",&pass);
+        pass==1999?printf("Correct\n"):printf("Wrong\n");
+    }
+    return 0;
+}
