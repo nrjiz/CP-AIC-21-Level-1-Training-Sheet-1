153723beea762a9f63680848c534b581d918dadb
nrjiz <164118742+nrjiz@users.noreply.github.com>
2024-07-17 14:30:22 +0300

    Update and rename J - Primes from 1 to n to J - Primes from 1 to n.cpp

--- sheet#2/J - Primes from 1 to n.cpp
+++ sheet#2/J - Primes from 1 to n.cpp
@@ -1,3 +1,21 @@
+#include<iostream>
+#include<cmath>
+bool isPrime(int numb){
+    for(int i = 2;i<=sqrt(numb);++i ){
+       if(numb%i==0)return false; 
+    }
+    return true;
+}
+int main(){
+    int input;
+    scanf("%d",&input);
+    int counter=2;
+    while(counter<=input){
+        !isPrime(counter)?:printf("%d ",counter);
+        ++counter;
+    }
+    return 0;
+}
 ////////////////////////////////////////////////////
 
 #include<iostream>
