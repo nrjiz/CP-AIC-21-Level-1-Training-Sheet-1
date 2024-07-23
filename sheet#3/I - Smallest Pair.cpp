a89fc39ed6a67a54779ec73c6d4aa9c1e159793c
nrjiz <164118742+nrjiz@users.noreply.github.com>
2024-07-23 10:22:50 +0300

    Update I - Smallest Pair.cpp

--- sheet#3/I - Smallest Pair.cpp
+++ sheet#3/I - Smallest Pair.cpp
@@ -1,3 +1,36 @@
+//O(n)solution:
+#include <iostream>
+#include <vector>
+#include <limits>
+ 
+int main() {
+    int T;
+    std::cin >> T;
+    while (T--) {
+        int N;
+        std::cin >> N;
+        std::vector<int> arr(N);
+        for (int i = 0; i < N; ++i) {
+            std::cin >> arr[i];
+        }
+ 
+        int minAiMinusI = std::numeric_limits<int>::max();
+        int minResult = std::numeric_limits<int>::max();
+ 
+        for (int j = 1; j < N; ++j) {
+            minAiMinusI = std::min(minAiMinusI, arr[j - 1] - (j - 1));
+            int result = arr[j] + j + minAiMinusI;
+            minResult = std::min(minResult, result);
+        }
+ 
+        std::cout << minResult << "\n";
+    }
+    return 0;
+}
+
+
+
+///////
 #include<iostream>
 #include<limits>
 #include<vector>
